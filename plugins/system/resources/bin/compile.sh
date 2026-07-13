602cdcf26fb2ab2b34f3f4afcf56ab5ee68bd939
Cristobal Mejia <cristobal.mejia@gmail.com>
2026-07-13 16:59:30 -0600

    -Xlint:-deprecation

--- plugins/system/resources/bin/compile.sh
+++ plugins/system/resources/bin/compile.sh
@@ -6,7 +6,7 @@ find plugins/system/code plugins/openedit/code \
 	-exec cp --parents {} build \;
 echo "Compiling Java code..." 
 javac -g -d build \
-	--source 21 --target 21 -nowarn \
+	--source 21 --target 21 -nowarn -Xlint:-deprecation \
 	-classpath "$(find plugins/system/lib plugins/finder/lib plugins/community/lib \
 		-type f \( -name '*.jar' -o -path '*/compile/*.jar' \) | tr '\n' ':')" \
 	$(find plugins/openedit/code plugins/system/code plugins/finder/code plugins/community/code \
